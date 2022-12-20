e550a9105d37ed5ee233ce9102bfc22e9bee1f18
ceydamehves <ceydamehves@gmail.com>
2022-12-20 23:23:59 +0300

    some texts added

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,7 +3,6 @@ import {
   SafeAreaView,
   View,
   StyleSheet,
-  StatusBar,
   Text,
   TextInput,
   TouchableOpacity
@@ -16,10 +15,20 @@ function MainScreen ({ navigation }) {
   
   let strings = new LocalizedStrings({
     "en":{
-      convert:"Convert to Morse Code!"
+      enter:"Enter Text to be Converted to Morse Code : ",
+      convert:"Convert to Morse Code!",
+      please:"Please enter some text to convert",
+      reset:"Reset!",
+      converted:"Converted",
+      copy:"Copy!"
     },
     "tr":{
-      convert:"Mors Alfabesine Çevir!"
+      enter:"Mors Alfabesine Çevirmek İstediğiniz Metin : ",
+      convert:"Mors Alfabesine Çevir!",
+      please:"Lütfen çevrilmesini istediğiniz metni girin",
+      reset:"Sıfırla!",
+      converted:"Çeviri",
+      copy:"Kopyala!"
     }
   })
 
@@ -63,55 +72,62 @@ function MainScreen ({ navigation }) {
   const copy = () => Clipboard.setString(convertToMorse(text));
 
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:'#f1f1f1'}}>  
-      <View style={styles.responsiveView}> 
-          <View style={{margin:10, backgroundColor: '#F178B6', width: 300, height:40}}>
-            <TextInput
+    <SafeAreaView style={styles.responsiveView}>        
+      <Text style={{color:'#434764', fontSize:16}}> {strings.enter} </Text>
+          <View style={{margin:10, backgroundColor:"#fff", borderColor: '#A89AF0', borderRadius: 7, borderLeftWidth: 0.9, borderRightWidth: 0.9, borderBottomWidth: 1.7, borderTopWidth: 0.3, width: 300, height:40}}>
+            <TextInput 
             style={styles.input}
             onChangeText={onChangeText}
             value={text}
             clearButtonMode='always'
-            cursorColor="blue"
+            cursorColor="#A89AF0"
             inlineImageLeft='search_icon'
             />
           </View>
-          <TouchableOpacity onPress={() => setShowConverted(!showConverted)} style={{backgroundColor:'#F178B6', height: 30, width: 190, alignItems: 'center', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => setShowConverted(!showConverted)} style={{margin:10, borderColor: '#A89AF0', borderRadius: 1, backgroundColor:'#A89AF0', height: 30, width: 190, alignItems: 'center', justifyContent: 'center', borderRadius: 10}}>
             <Text style={{color:'#fff', fontSize:16}}> {strings.convert} </Text>
           </TouchableOpacity>
           {
             showConverted == false ?
             <View>
-              <Text> please enter some text to convert </Text>
+              <Text style={{color:'#434764', fontSize:16}}> {strings.please} </Text>
             </View> 
             :
             showConverted == true ?
-            <View>
-              <Text> converted : {convertToMorse(text)} </Text>
-              <TouchableOpacity  onPress={() => copy()}> 
-                <Text>copy</Text> 
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setShowConverted(!showConverted) & onChangeText("")} style={{backgroundColor:'#F178B6', height: 30, width: 190, alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{color:'#fff', fontSize:16}}>Reset!</Text>
-              </TouchableOpacity>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <View style={{flexDirection: 'column', alignItems: 'center'}}>
+                <Text style={{color:'#434764', fontSize:18}}> {strings.converted} </Text>
+                <Text style={{color:'#434764', fontSize:18}}>  {convertToMorse(text)} </Text>
+              </View>              
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <TouchableOpacity  onPress={() => copy()} style={{margin:10, borderColor: '#A89AF0', borderRadius: 1, backgroundColor:'#A89AF0', height: 30, width: 100, alignItems: 'center', justifyContent: 'center', borderRadius: 15}}>
+                  <Text style={{color:'#fff', fontSize:16}}> {strings.copy} </Text> 
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setShowConverted(!showConverted) & onChangeText("")} style={{margin:10, borderColor: '#A89AF0', borderRadius: 1, backgroundColor:'#A89AF0', height: 30, width: 100, alignItems: 'center', justifyContent: 'center', borderRadius: 15}}>
+                  <Text style={{color:'#fff', fontSize:16}}> {strings.reset} </Text>
+                </TouchableOpacity>
+              </View>
             </View>             
             :
             <View>
               <Text> error </Text>
             </View> 
           }
-          
-      </View>   
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   responsiveView:{
-    alignItems:'center', 
-    justifyContent: 'center'
+    flex:1, 
+    backgroundColor:'#F0F0FF', 
+    justifyContent: 'center', 
+    alignItems: 'center'
   },
   input: {
-    color: '#FFF'
+    color: '#434764',
+    fontSize: 14,
+    marginLeft: 5
   }
 });
 
